1a446c21967ce2e15d49538dcf5cb23aeb70c996
Daniel <daniel@becher.com.br>
2024-02-05 16:46:38 -0300

    criação de modo dev e criação de autenticação para a API

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,18 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import productsController from './src/controllers/products.js'
 
+import basicAuth from 'express-basic-auth'
+
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(basicAuth({
+    users: {
+        "admininastro": "sacol@um@123"
+    }
+}))
 app.use(bodyParser.json())
-
 app.use('/', productsController)
-
 app.listen(port, () => {
     console.log(`Server listening to port ${port}`)
 })
